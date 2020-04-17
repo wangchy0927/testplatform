66b9e683522a53deaefcb3c037522fb9b0c8e82f
e <wangchy0927@sina.com>
2020-04-17 17:34:50 +0800

    '修改get的参数传递方式'

--- src/main/java/com/miracle/controller/TestController.java
+++ src/main/java/com/miracle/controller/TestController.java
@@ -8,10 +8,7 @@ import com.miracle.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -66,8 +63,8 @@ public class TestController {
         logger.info(test.getName()+"-"+test.toString());
     }
 
-    @RequestMapping(value = "/getOrdersByCId",method = RequestMethod.GET)
-    public Customer getOrdersByCId(int id){
+    @RequestMapping(value = "/getOrdersByCId/{id}",method = RequestMethod.GET)
+    public Customer getOrdersByCId(@PathVariable("id") int id){
         return customerService.getOrdersByCId(id);
     }
 
